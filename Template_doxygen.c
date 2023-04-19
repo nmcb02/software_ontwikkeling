32ea7b2ba1b0116a3133fb83a5e56f9e22df09e0
Skip <skip@wtfman.nl>
2023-04-19 11:12:07 +0200

    Add Template Doxygen

---
+++ Template_doxygen.c
@@ -0,0 +1,17 @@
+/*********************************************************
+ * @file: Template_doxygen.c
+ * 
+ * @brief: A test file for doxygen documentation
+ * 
+ * @authors: Skip Wijtman
+ * @date: 19-4-2023
+*********************************************************/
+
+
+/****
+ * @brief: Test fuction header
+ * 
+ * @param: Nothing
+ * 
+ * @return: Nothing
+****/
