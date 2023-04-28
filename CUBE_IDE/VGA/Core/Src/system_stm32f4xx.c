9b70caec8053eb6293ee13b1ef20557cfd02e541
Skip <skip@wtfman.nl>
2023-04-28 10:50:11 +0200

    SWD-1: fuck up, IOC file changes the whole file to be used with HAL drivers, we dont use those. So NEVER open/use the IOC, we dont need it.

--- CUBE_IDE/VGA/Core/Src/system_stm32f4xx.c
+++ CUBE_IDE/VGA/Core/Src/system_stm32f4xx.c
@@ -391,7 +391,7 @@ static void SetSysClock(void)
     RCC->CFGR |= RCC_CFGR_SW_PLL;
 
     /* Wait till the main PLL is used as system clock source */
-    while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS ) != RCC_CFGR_SWS_PLL);
+    while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS ) != RCC_CFGR_SWS_PLL)
     {
     }
   }
@@ -621,17 +621,17 @@ void SystemInit_ExtMemCtl(void)
 //
 ///************************* Miscellaneous Configuration ************************/
 ///*!< Uncomment the following line if you need to use external SRAM or SDRAM as data memory  */
-//#if defined(STM32F405xx) || defined(STM32F415xx) || defined(STM32F407xx) || defined(STM32F417xx)\
-// || defined(STM32F427xx) || defined(STM32F437xx) || defined(STM32F429xx) || defined(STM32F439xx)\
+//#if defined(STM32F405xx) || defined(STM32F415xx) || defined(STM32F407xx) || defined(STM32F417xx)
+// || defined(STM32F427xx) || defined(STM32F437xx) || defined(STM32F429xx) || defined(STM32F439xx)
 // || defined(STM32F469xx) || defined(STM32F479xx) || defined(STM32F412Zx) || defined(STM32F412Vx)
 ///* #define DATA_IN_ExtSRAM */
-//#endif /* STM32F40xxx || STM32F41xxx || STM32F42xxx || STM32F43xxx || STM32F469xx || STM32F479xx ||\
+//#endif /* STM32F40xxx || STM32F41xxx || STM32F42xxx || STM32F43xxx || STM32F469xx || STM32F479xx ||
 //          STM32F412Zx || STM32F412Vx */
 //
-//#if defined(STM32F427xx) || defined(STM32F437xx) || defined(STM32F429xx) || defined(STM32F439xx)\
+//#if defined(STM32F427xx) || defined(STM32F437xx) || defined(STM32F429xx) || defined(STM32F439xx)
 // || defined(STM32F446xx) || defined(STM32F469xx) || defined(STM32F479xx)
 ///* #define DATA_IN_ExtSDRAM */
-//#endif /* STM32F427xx || STM32F437xx || STM32F429xx || STM32F439xx || STM32F446xx || STM32F469xx ||\
+//#endif /* STM32F427xx || STM32F437xx || STM32F429xx || STM32F439xx || STM32F446xx || STM32F469xx ||
 //          STM32F479xx */
 //
 ///*!< Uncomment the following line if you need to relocate your vector Table in
@@ -798,7 +798,7 @@ void SystemInit_ExtMemCtl(void)
 //}
 //
 //#if defined (DATA_IN_ExtSRAM) && defined (DATA_IN_ExtSDRAM)
-//#if defined(STM32F427xx) || defined(STM32F437xx) || defined(STM32F429xx) || defined(STM32F439xx)\
+//#if defined(STM32F427xx) || defined(STM32F437xx) || defined(STM32F429xx) || defined(STM32F439xx)
 // || defined(STM32F469xx) || defined(STM32F479xx)
 ///**
 //  * @brief  Setup the external memory controller.
@@ -974,7 +974,7 @@ void SystemInit_ExtMemCtl(void)
 //void SystemInit_ExtMemCtl(void)
 //{
 //  __IO uint32_t tmp = 0x00;
-//#if defined(STM32F427xx) || defined(STM32F437xx) || defined(STM32F429xx) || defined(STM32F439xx)\
+//#if defined(STM32F427xx) || defined(STM32F437xx) || defined(STM32F429xx) || defined(STM32F439xx)
 // || defined(STM32F446xx) || defined(STM32F469xx) || defined(STM32F479xx)
 //#if defined (DATA_IN_ExtSDRAM)
 //  register uint32_t tmpreg = 0, timeout = 0xFFFF;
@@ -1066,7 +1066,7 @@ void SystemInit_ExtMemCtl(void)
 //  /* No pull-up, pull-down for PGx pins */
 //  GPIOG->PUPDR   = 0x00000000;
 //
-//#if defined(STM32F427xx) || defined(STM32F437xx) || defined(STM32F429xx) || defined(STM32F439xx)\
+//#if defined(STM32F427xx) || defined(STM32F437xx) || defined(STM32F429xx) || defined(STM32F439xx)
 // || defined(STM32F469xx) || defined(STM32F479xx)
 //  /* Connect PHx pins to FMC Alternate function */
 //  GPIOH->AFR[0]  = 0x00C0CC00;
@@ -1165,8 +1165,8 @@ void SystemInit_ExtMemCtl(void)
 //#endif /* DATA_IN_ExtSDRAM */
 //#endif /* STM32F427xx || STM32F437xx || STM32F429xx || STM32F439xx || STM32F446xx || STM32F469xx || STM32F479xx */
 //
-//#if defined(STM32F405xx) || defined(STM32F415xx) || defined(STM32F407xx) || defined(STM32F417xx)\
-// || defined(STM32F427xx) || defined(STM32F437xx) || defined(STM32F429xx) || defined(STM32F439xx)\
+//#if defined(STM32F405xx) || defined(STM32F415xx) || defined(STM32F407xx) || defined(STM32F417xx)
+// || defined(STM32F427xx) || defined(STM32F437xx) || defined(STM32F429xx) || defined(STM32F439xx)
 // || defined(STM32F469xx) || defined(STM32F479xx) || defined(STM32F412Zx) || defined(STM32F412Vx)
 //
 //#if defined(DATA_IN_ExtSRAM)
@@ -1244,7 +1244,7 @@ void SystemInit_ExtMemCtl(void)
 //  FMC_Bank1->BTCR[3]  = 0x00110212;
 //  FMC_Bank1E->BWTR[2] = 0x0fffffff;
 //#endif /* STM32F469xx || STM32F479xx */
-//#if defined(STM32F405xx) || defined(STM32F415xx) || defined(STM32F407xx)|| defined(STM32F417xx)\
+//#if defined(STM32F405xx) || defined(STM32F415xx) || defined(STM32F407xx)|| defined(STM32F417xx)
 //   || defined(STM32F412Zx) || defined(STM32F412Vx)
 //  /* Delay after an RCC peripheral clock enabling */
 //  tmp = READ_BIT(RCC->AHB3ENR, RCC_AHB3ENR_FSMCEN);
@@ -1255,7 +1255,7 @@ void SystemInit_ExtMemCtl(void)
 //#endif /* STM32F405xx || STM32F415xx || STM32F407xx || STM32F417xx || STM32F412Zx || STM32F412Vx */
 //
 //#endif /* DATA_IN_ExtSRAM */
-//#endif /* STM32F405xx || STM32F415xx || STM32F407xx || STM32F417xx || STM32F427xx || STM32F437xx ||\
+//#endif /* STM32F405xx || STM32F415xx || STM32F407xx || STM32F417xx || STM32F427xx || STM32F437xx ||
 //          STM32F429xx || STM32F439xx || STM32F469xx || STM32F479xx || STM32F412Zx || STM32F412Vx  */
 //  (void)(tmp);
 //}
