516fec8407e295a48678b7830db05115b6a7085b
Naomi <naomimcborn@gmail.com>
2023-05-17 10:14:17 +0200

    SWD-10: add API_function files

---
+++ CUBE_IDE/VGA/Core/Inc/API_functions.h
@@ -0,0 +1,13 @@
+/*
+ * API_functions.h
+ *
+ *  Created on: May 17, 2023
+ *      Author: naomi
+ */
+
+#ifndef INC_API_FUNCTIONS_H_
+#define INC_API_FUNCTIONS_H_
+
+
+
+#endif /* INC_API_FUNCTIONS_H_ */
---
+++ CUBE_IDE/VGA/Core/Src/API_functions.c
@@ -0,0 +1,8 @@
+/*
+ * API_functions.c
+ *
+ *  Created on: May 17, 2023
+ *      Author: naomi
+ */
+
+
