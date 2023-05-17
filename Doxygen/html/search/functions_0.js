71bc6b3cb994f9b6afd81dbe8c4c167947c65c25
Naomi <naomimcborn@gmail.com>
2023-05-17 14:55:46 +0200

    SWD-11: Update doxygen

--- Doxygen/html/search/functions_0.js
+++ Doxygen/html/search/functions_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['api_5fdraw_5fline_0',['API_draw_line',['../_a_p_i__functions_8h.html#a5762291bd9ba0b7c55877a82192975c9',1,'API_draw_line(int x_1, int y_1, int x_2, int y_2, int color, int weight):&#160;API_functions.c'],['../_a_p_i__functions_8c.html#a5762291bd9ba0b7c55877a82192975c9',1,'API_draw_line(int x_1, int y_1, int x_2, int y_2, int color, int weight):&#160;API_functions.c']]]
+  ['api_5fdraw_5fline_0',['API_draw_line',['../_a_p_i__functions_8h.html#a5762291bd9ba0b7c55877a82192975c9',1,'API_draw_line(int x_1, int y_1, int x_2, int y_2, int color, int weight):&#160;API_functions.c'],['../_a_p_i__functions_8c.html#a5762291bd9ba0b7c55877a82192975c9',1,'API_draw_line(int x_1, int y_1, int x_2, int y_2, int color, int weight):&#160;API_functions.c']]],
+  ['api_5fdraw_5frectangle_1',['API_draw_rectangle',['../_a_p_i__functions_8h.html#a0587bfc121fd81d7f77001ded7166d4f',1,'API_draw_rectangle(int x, int y, int width, int height, int color, int filled):&#160;API_functions.c'],['../_a_p_i__functions_8c.html#a873d84dd7c5f171783799bcd36ac7688',1,'API_draw_rectangle(int x_1, int y_1, int width, int height, int color, int filled):&#160;API_functions.c']]]
 ];
