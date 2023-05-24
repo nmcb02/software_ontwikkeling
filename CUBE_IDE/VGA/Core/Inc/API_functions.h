973bb26717972509cc361f6d51d1f3d1806e2d51
Naomi <naomimcborn@gmail.com>
2023-05-24 14:14:15 +0200

    SWD-18: add bitmap function define and comments for doxygen

--- CUBE_IDE/VGA/Core/Inc/API_functions.h
+++ CUBE_IDE/VGA/Core/Inc/API_functions.h
@@ -22,6 +22,7 @@
 
 // prototype user functions
 int API_draw_line(int x_1, int y_1, int x_2, int y_2, int color, int weight);
-int API_draw_rectangle (int x, int y, int width, int height, int color, int filled);
+int API_clearscreen (int color);
+int API_draw_bitmap (int x_lup, int y_lup, int bm_nr);
 
 #endif /* INC_API_FUNCTIONS_H_ */
--- CUBE_IDE/VGA/Core/Src/API_functions.c
+++ CUBE_IDE/VGA/Core/Src/API_functions.c
@@ -5,7 +5,7 @@
  *
  * @authors Naomi Born
  * @date    17-05-2023
- * @version 1.1
+ * @version 1.0
 *********************************************************/
 //#include other "user-header"
 #include "API_functions.h"
@@ -20,7 +20,7 @@
  * @param   color Color of the line
  * @param   weight Width of the line
  *
- * @return  Error code if error occurs
+ * @return  Error code if error or no error occurs
 *******************************************************/
 int API_draw_line(int x_1, int y_1, int x_2, int y_2, int color, int weight)
 {
@@ -45,8 +45,10 @@ int API_draw_line(int x_1, int y_1, int x_2, int y_2, int color, int weight)
 	{
 		for (int y = y_1; y <= y_1 + weight; y++)
 		{
-			for (int x = x_1; x <= x_2; x++)
+			for(int x = x_1; x <= x_2; x++)
+			{
 				UB_VGA_SetPixel(x, y, color);
+			}
 		}
 	}
 
@@ -54,14 +56,16 @@ int API_draw_line(int x_1, int y_1, int x_2, int y_2, int color, int weight)
 	{
 		for (int x = x_1; x <= x_1 + weight; x++)
 		{
-			for (int y = y_1; y <= y_2; y++)
+			for(int y = y_1; y <= y_2; y++)
+			{
 				UB_VGA_SetPixel(x, y, color);
+			}
 		}
 	}
 
 	else if (dxabs>=dyabs)	// Delta x is bigger than delta y
 	{
-		for (int j = 0; j < weight; j++)
+		for(int j = 0; j < weight; j++)
 		{
 			for (int i = 0; i < dxabs; i++) // Loop for a line < delta x
 			{
@@ -83,7 +87,7 @@ int API_draw_line(int x_1, int y_1, int x_2, int y_2, int color, int weight)
 
 	else
 	{
-		for (int j = 0; j < weight; j++)
+		for(int j = 0; j < weight; j++)
 		{
 			for (int i = 0; i < dyabs; i++) // Loop for a line < delta x
 			{
@@ -105,47 +109,30 @@ int API_draw_line(int x_1, int y_1, int x_2, int y_2, int color, int weight)
 }
 
 /*****************************************************//**
- * @brief   This function gives the user the ability to draw a rectangle on a VGA screen
+ * @brief   This function gives the user the ability to clear the screen to a certain color
  *
- * @param   x_1 Starting point coördinate of x
- * @param   y_1 Starting point coördinate of y
- * @param   width Width of the rectangle max. 320
- * @param   height Height of the rectangle max. 240
- * @param   color Color of the rectangle/borders
- * @param   filled 1 is filled rectangle 0 is just borders
+ * @param   color Color of the screen
  *
- * @return  Error code if error occurs
+ * @return  Error code if error or no error occurs
 *******************************************************/
-int API_draw_rectangle (int x_1, int y_1, int width, int height, int color, int filled)
+int API_clearscreen (int color)
 {
-	int x_2, y_2;
+	UB_VGA_FillScreen(color);
 
-	x_2 = x_1 + width;
-	y_2 = y_1 + height;
+	return 0;
+}
 
-	if (filled)	//Drawing a filled rectangle
-	{
-		for (int y = y_1; y <= y_2; y++)
-		{
-			for (int x = x_1; x <= width; x++)
-				UB_VGA_SetPixel(x, y, color);
-		}
-	}
-
-	else if (!filled)
-	{
-		for (int x = x_1; x <= x_2; x++)	// Draw top line
-			UB_VGA_SetPixel(x, y_1, color);
-
-		for (int y = y_1; y <= y_2; y++)	// Draw left line
-			UB_VGA_SetPixel(x_1, y, color);
-
-		for (int y = y_1; y <= y_2; y++)	// Draw right line
-			UB_VGA_SetPixel(x_2, y, color);
-
-		for (int x = x_1; x <= x_2; x++)	// Draw bottom line
-			UB_VGA_SetPixel(x, y_2, color);
-	}
+/*****************************************************//**
+ * @brief   This function gives the user the ability to put a bitmap on the VGA screen
+ *
+ * @param   x_lup X coordinate of x left up
+ * 			y_lup Y coordinate of y left up
+ * 			bm_nr Number of the bitmap
+ *
+ * @return  Error code if error or no error occurs
+*******************************************************/
+int API_draw_bitmap (int x_lup, int y_lup, int bm_nr)
+{
 
 	return 0;
 }
