388718e4873136c6dc1c83a8f00671321af1033d
Naomi <naomimcborn@gmail.com>
2023-05-31 13:09:56 +0200

    SWD-19: Add error nr to draw line

--- CUBE_IDE/VGA/Core/Src/API_functions.c
+++ CUBE_IDE/VGA/Core/Src/API_functions.c
@@ -26,82 +26,94 @@ int API_draw_line(int x_1, int y_1, int x_2, int y_2, int color, int weight)
 {
 	int dx, dy, sdx, sdy, dxabs, dyabs, x, y, px, py;
 
-	dx = x_2 - x_1;	// Calculate deltas of x and y
-	dy = y_2 - y_1;
+	if ((0 > x_1 > VGA_DISPLAY_X)||(0 > x_2 > VGA_DISPLAY_X))
+		return 1; // ERROR:	Invalid value
 
-	dxabs = abs(dx); // Absolute val so no negative values exist
-	dyabs = abs(dy);
+	else if ((0 > y_1 > VGA_DISPLAY_Y)||(0 > y_2 > VGA_DISPLAY_Y))
+		return 1; // ERROR:	Invalid value
 
-	sdx = sgn(dx);
-	sdy = sgn(dy);
-
-	x = dyabs>>1; // Bit shift to right aka dyabs/2
-	y = dxabs>>1; // Bit shift to right aka dxabs/2
-
-	px = x_1;
-	py = y_1;
-
-	if (dy == 0)	// Horizontal line
-	{
-		for (int y = y_1; y <= y_1 + weight; y++)
-		{
-			for (int x = x_1; x <= x_2; x++)
-				UB_VGA_SetPixel(x, y, color);
-		}
-	}
-
-	else if (dx == 0) // vertical line
-	{
-		for (int x = x_1; x <= x_1 + weight; x++)
-		{
-			for (int y = y_1; y <= y_2; y++)
-				UB_VGA_SetPixel(x, y, color);
-		}
-	}
-
-	else if (dxabs>=dyabs)	// Delta x is bigger than delta y
-	{
-		for (int j = 0; j < weight; j++)
-		{
-			for (int i = 0; i < dxabs; i++) // Loop for a line < delta x
-			{
-				y += dyabs;
-
-				if (y >= dxabs)
-				{
-					y -= dxabs;
-					py += sdy;
-				}
-
-				px += sdx;
-				UB_VGA_SetPixel(px+j, py, color);
-			}
-			px = x_1;
-			py = y_1;
-		}
-	}
+	else if ((0 > weight > VGA_DISPLAY_X)||(0 > weight > VGA_DISPLAY_Y))
+		return 1; // ERROR:	Invalid value
 
 	else
 	{
-		for (int j = 0; j < weight; j++)
-		{
-			for (int i = 0; i < dyabs; i++) // Loop for a line < delta x
-			{
-				x += dxabs;
+		dx = x_2 - x_1;	// Calculate deltas of x and y
+		dy = y_2 - y_1;
 
-				if (x >= dyabs)
-				{
-					x -= dyabs;
-					px += sdx;
-				}
-				py += sdy;
-				UB_VGA_SetPixel(px+j, py, color);
+		dxabs = abs(dx); // Absolute val so no negative values exist
+		dyabs = abs(dy);
+
+		sdx = sgn(dx);
+		sdy = sgn(dy);
+
+		x = dyabs>>1; // Bit shift to right aka dyabs/2
+		y = dxabs>>1; // Bit shift to right aka dxabs/2
+
+		px = x_1;
+		py = y_1;
+
+		if (dy == 0)	// Horizontal line
+		{
+			for (int y = y_1; y <= y_1 + weight; y++)
+			{
+				for (int x = x_1; x <= x_2; x++)
+					UB_VGA_SetPixel(x, y, color);
 			}
-			px = x_1;
-			py = y_1;
 		}
+
+		else if (dx == 0) // vertical line
+		{
+			for (int x = x_1; x <= x_1 + weight; x++)
+			{
+				for (int y = y_1; y <= y_2; y++)
+					UB_VGA_SetPixel(x, y, color);
+			}
+		}
+
+		else if (dxabs>=dyabs)	// Delta x is bigger than delta y
+		{
+			for (int j = 0; j < weight; j++)
+			{
+				for (int i = 0; i < dxabs; i++) // Loop for a line < delta x
+				{
+					y += dyabs;
+
+					if (y >= dxabs)
+					{
+						y -= dxabs;
+						py += sdy;
+					}
+
+					px += sdx;
+					UB_VGA_SetPixel(px+j, py, color);
+				}
+				px = x_1;
+				py = y_1;
+			}
+		}
+
+		else
+		{
+			for (int j = 0; j < weight; j++)
+			{
+				for (int i = 0; i < dyabs; i++) // Loop for a line < delta x
+				{
+					x += dxabs;
+
+					if (x >= dyabs)
+					{
+						x -= dyabs;
+						px += sdx;
+					}
+					py += sdy;
+					UB_VGA_SetPixel(px+j, py, color);
+				}
+				px = x_1;
+				py = y_1;
+			}
+		}
+		return 0;
 	}
-	return 0;
 }
 
 /*****************************************************//**
